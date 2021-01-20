3e32c71d6cfd14ba34935ba247f53e958bdf4adf
unknown <filip.norstrom@student.kyh.se>
2021-01-20 11:09:39 +0100

    my first push

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1>Hello</h1>
+  );
+}
+
+function test() {
+  return (
+    <p>test</p>
+  );
+}
+
+export default App;
