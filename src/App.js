7207a676cffbb412471d93c13881af811eeb633d
unknown <filip.norstrom@student.kyh.se>
2021-01-20 11:16:19 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <h1>Hello</h1>
+    
   );
 }
 
-function test() {
-  return (
-    <p>test</p>
-  );
-}
 
 export default App;
